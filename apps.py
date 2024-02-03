21a5cb69ff9d3e967d9c517f6ce4e46644a2498d
Rafael Terra <rafaelstjf@hotmail.com>
2024-02-03 17:27:28 -0300

    run one raxml per gene

--- apps.py
+++ apps.py
@@ -1,10 +1,87 @@
 from pycompss.api.task import task
 from pycompss.api.binary import binary
-from pycompss.api.parameter import *
+from pycompss.api.parameter import DIRECTORY_INOUT
 
 @binary(binary="raxmlHPC",
         working_dir=".",
-        args= "-s {{alignment}} -m {{evo_model}} -f a -x {{seed}} -p {{seed}} -N {{bs_value}} -n {{out_suffix}}") # decorador para binários externos. O nome do programa deve ser passado como parâmetro
+        args= "-s {{alignment}} -m {{evo_model}} --HKY85 -f a -x {{seed}} -p {{seed}} -N {{bs_value}} -n {{out_suffix}} -w {{working_dir}}") # decorador para binários externos. O nome do programa deve ser passado como parâmetro
 @task() # mesmo com o decorador tem-se que colocar o decorador de task
-def raxml(alignment, evo_model, bs_value, out_suffix, seed):
+def raxml(alignment, evo_model, bs_value, out_suffix, seed, working_dir):
     pass
+
+@task(basedir=DIRECTORY_INOUT)
+def setup_phylip_data(basedir, test):
+    """Extract the sequence alignments tar file and convert the gene alignments from the nexus format to the phylip format.
+
+    Parameters:
+            basedir: it is going to search for a tar file with nexus files. The script will create:
+            seqdir=input/nexus
+            seqdir=input/phylip
+    Returns:
+        returns an parsl's AppFuture.
+
+    Raises:
+        PhylipMissingData --- if cannot find a tar file with nexus files.
+
+
+    TODO: Provide provenance.
+    NB:
+        Stdout and Stderr are defaulted to parsl.AUTO_LOGNAME, so the log will be automatically 
+        named according to task id and saved under task_logs in the run directory.
+    """
+    import os, glob, tarfile, tarfile, shutil
+    from Bio import AlignIO
+    from pathlib import Path
+    input_dir = os.path.join(basedir, 'input')
+    sequence_dir = os.path.join(input_dir, 'sequence')
+    input_format = 0
+    # First the sequences are extracted
+    Path(sequence_dir).mkdir(exist_ok=True)
+    if os.path.exists(sequence_dir):
+        tar_file = basedir['sequences']
+        tar = tarfile.open(tar_file, "r:gz")
+        tar.extractall(path=sequence_dir)
+    # Now one file is opened to check its format
+    sequences = glob.glob(os.path.join(sequence_dir, '*'))
+    if len(sequences) == 0:
+        pass # TODO raise an exception
+    with open(sequences[0], 'r') as s_file:
+        line = s_file.readline()
+        if "#NEXUS" in line:
+            input_format = 0 # nexus
+        elif ">" in line:
+            input_format = 1 # fasta
+        else:
+            input_format = 2 # other .i.e. phylip
+
+    input_nexus_dir = os.path.join(input_dir, 'nexus')
+    input_phylip_dir = os.path.join(input_dir, 'phylip')
+    input_fasta_dir = os.path.join(input_dir, 'fasta')
+    # So, some work must be done. Build the Nexus directory
+    if not os.path.isdir(input_nexus_dir):
+        Path(input_nexus_dir).mkdir(exist_ok=True)
+    if not os.path.isdir(input_phylip_dir):
+        Path(input_phylip_dir).mkdir(exist_ok=True)
+    if not os.path.isdir(input_fasta_dir):
+        Path(input_fasta_dir).mkdir(exist_ok=True)
+    # Now, use the function to convert nexus to phylip.
+    files = glob.glob(os.path.join(sequence_dir,'*'))
+    try:
+        for f in files:
+            out_name = os.path.basename(f).split('.')[0]
+            if input_format == 0:
+                AlignIO.convert(f, "nexus", os.path.join(input_phylip_dir, f'{out_name}.phy'), "phylip-sequential", molecule_type = "DNA")
+                AlignIO.convert(f, "nexus", os.path.join(input_phylip_dir, f'{out_name}.fasta'), "fasta", molecule_type = "DNA")
+                shutil.copyfile(f, os.path.join(input_nexus_dir, os.path.basename(f)))
+            if input_format == 1:
+                AlignIO.convert(f, "fasta", os.path.join(input_phylip_dir, f'{out_name}.phy'), "phylip-sequential", molecule_type = "DNA")
+                AlignIO.convert(f, "fasta", os.path.join(input_nexus_dir, f'{out_name}.nex'), "nexus", molecule_type = "DNA")
+                shutil.copyfile(f, os.path.join(input_fasta_dir, os.path.basename(f)))
+            if input_format == 2:
+                AlignIO.convert(f, "phylip-sequential", os.path.join(input_nexus_dir, f'{out_name}.nex'), "nexus", molecule_type = "DNA")
+                AlignIO.convert(f, "phylip-sequential", os.path.join(input_fasta_dir, f'{out_name}.fasta'), "fasta", molecule_type = "DNA")
+                shutil.copyfile(f, os.path.join(input_phylip_dir, os.path.basename(f)))
+    except Exception as e:
+       pass # TODO raise an exception
+    return
+
--- workflow.py
+++ workflow.py
@@ -1,7 +1,19 @@
 from pycompss.api.api import compss_wait_on
 import apps
-
+import glob, os
+def raxml_snaq(dataset):
+    #get the list of genes
+    input_folder = os.path.join(os.path.join(dataset,"input"), "phylip")
+    raxml_folder = os.path.join(dataset, "raxml")
+    genes = glob.glob(os.path.join(input_folder, "*.phy"))
+    r_raxml = list()
+    for gene in genes:
+        suffix = os.path.splitext(os.path.basename(gene))[0]
+        r = apps.raxml(alignment=gene, evo_model="GTRGAMMA",
+                   bs_value="100", out_suffix=suffix, seed="123", working_dir=raxml_folder)
+        r_raxml.append(r)
+    apps.setup_phylip_data()
 if __name__ == "__main__":
-    r = apps.raxml(alignment="msa_artigo_denv.phy", evo_model="GTRGAMMA --HKY85",
-                   bs_value="100", out_suffix="saida", seed="123")
-    compss_wait_on(r)
+    raxml_snaq(os.path.join(os.getcwd(), "Denv_1_outgroup"))
+    
+    
