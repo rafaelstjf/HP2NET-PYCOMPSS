0206969cdf67bddbeb80e2303825e014234c2b67
Rafael Terra <rafaelstjf@hotmail.com>
2024-02-06 17:34:13 -0300

    now the whole pipeline is executed

--- apps.py
+++ apps.py
@@ -330,15 +330,16 @@ def setup_astral(basedir, config, setup):
     r_dict["astral_output"] = astral_output
     return r_dict
 
-@binary(binary="java -jar /prj/posgrad/rafaelst/Documentos/GitHub/ASTRAL/Astral/astral.5.7.8.jar",
+@binary(binary="java",
          working_dir='.',
-         args=f"-i {{tree_output}} -b {{bs_file}} -r {{bs_value}} -o {{astral_output}}")
+         args="-jar /prj/posgrad/rafaelst/Documentos/GitHub/ASTRAL/Astral/astral.5.7.8.jar -i {{tree_output}} -b {{bs_file}} -r {{bs_value}} -o {{astral_output}}")
 @task()
 def astral(tree_output, bs_file, bs_value, astral_output):
     pass
 
-@julia(script=f"julia /prj/posgrad/rafaelst/Documentos/GitHub/HP2NET-PYCOMPSS/scripts/snaq.jl",
-        working_dir = '.')
+@binary(binary="julia",
+        working_dir = '.',
+        args="/prj/posgrad/rafaelst/Documentos/GitHub/HP2NET-PYCOMPSS/scripts/snaq.jl {{tree_method}} {{gen_tree}} {{spec_tree}} {{output_folder}} {{num_threads}} {{hmax}} {{runs}}")
 @task(astral=IN)
 def snaq(tree_method, gen_tree, spec_tree, output_folder, num_threads, hmax, runs, astral):
     pass
--- workflow.py
+++ workflow.py
@@ -9,6 +9,7 @@ def main():
     cf = bioconfig.ConfigFactory(config_file)
     config = cf.build_config()
     basedir = (config.workload)[0]
+    """
     folder_list = [config.raxml_dir, config.astral_dir, config.snaq_dir]
     r = apps.create_folders(basedir, config, folder_list)
     compss_wait_on(r)
@@ -20,19 +21,18 @@ def main():
         suffix = os.path.splitext(os.path.basename(g))[0]
         r_raxml.append(apps.raxml(alignment=g, evo_model=config.raxml_model,
                        bs_value=config.bootstrap, out_suffix=suffix, seed='123', working_dir=os.path.join(basedir["dir"], config.raxml_dir)))
-    """
     r_sad = apps.setup_tree_output(basedir, config, r_raxml)
     r_pastral = apps.setup_astral(basedir, config, r_sad)
-    r_astral = apps.astral(r_pastral["tree_output"], r_pastral["bs_file"], config.bootstrap, r_pastral["astral_output"])
+    """
+    r_pastral = apps.setup_astral(basedir, config, [])
+    r_pastral = compss_wait_on(r_pastral)
+    r_astral = apps.astral(tree_output = r_pastral["tree_output"], bs_file = r_pastral["bs_file"], bs_value = config.bootstrap, astral_output = r_pastral["astral_output"])
     raxml_dir = os.path.join(basedir['dir'], config.raxml_dir)
     besttree_file = os.path.join(raxml_dir, config.raxml_output)
     spec_tree = os.path.join(basedir['dir'], os.path.join(config.astral_dir, config.raxml_dir))
     spec_tree = os.path.join(spec_tree, config.astral_output)
     output_folder = os.path.join(basedir['dir'], config.snaq_dir)
-    r_snaq = apps.snaq(basedir["tree_method"], besttree_file, spec_tree, output_folder, "10", config.snaq_hmax, config.snaq_runs, r_astral)
-    compss_wait_on(r_snaq)
-    """
-
+    apps.snaq(tree_method = basedir["tree_method"], gen_tree = besttree_file, spec_tree = spec_tree, output_folder = output_folder, num_threads = 1, hmax = config.snaq_hmax, runs = config.snaq_runs, astral = r_astral)
 
 if __name__ == "__main__":
     main()
