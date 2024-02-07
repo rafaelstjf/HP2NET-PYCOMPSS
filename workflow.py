dc70cd068aac00d73f9296923f5d3616242a1c6b
Rafael Terra <rafaelstjf@hotmail.com>
2024-02-07 08:46:33 -0300

    identation

--- workflow.py
+++ workflow.py
@@ -19,21 +19,28 @@ def raxml_snaq():
     for g in genes["phylip"]:
         suffix = os.path.splitext(os.path.basename(g))[0]
         r_raxml.append(apps.raxml(alignment=g, evo_model=config.raxml_model,
-                       bs_value=config.bootstrap, out_suffix=suffix, seed='123', working_dir=os.path.join(basedir["dir"], config.raxml_dir)))
+                        bs_value=config.bootstrap, out_suffix=suffix, seed='123', 
+                        working_dir=os.path.join(basedir["dir"], 
+                        config.raxml_dir)))
     r_sad = apps.setup_tree_output(basedir, config, r_raxml)
     r_pastral = apps.setup_astral(basedir, config, r_sad)
-    r_pastral = apps.setup_astral(basedir, config, [])
     r_pastral = compss_wait_on(r_pastral)
-    r_astral = apps.astral(tree_output = r_pastral["tree_output"], bs_file = r_pastral["bs_file"], bs_value = config.bootstrap, astral_output = r_pastral["astral_output"])
+    r_astral = apps.astral(tree_output=r_pastral["tree_output"], bs_file=r_pastral["bs_file"],
+                           bs_value=config.bootstrap, astral_output=r_pastral["astral_output"])
     raxml_dir = os.path.join(basedir['dir'], config.raxml_dir)
     besttree_file = os.path.join(raxml_dir, config.raxml_output)
-    spec_tree = os.path.join(basedir['dir'], os.path.join(config.astral_dir, config.raxml_dir))
+    spec_tree = os.path.join(basedir['dir'], os.path.join(
+        config.astral_dir, config.raxml_dir))
     spec_tree = os.path.join(spec_tree, config.astral_output)
     output_folder = os.path.join(basedir['dir'], config.snaq_dir)
     r_snaq = list()
     for h in config.snaq_hmax:
-        print(h)
-        r_snaq.append(apps.snaq(tree_method = basedir["tree_method"], gen_tree = besttree_file, spec_tree = spec_tree, output_folder = output_folder, num_threads = 1, hmax = h, runs = config.snaq_runs, astral = r_astral))
+        r_snaq.append(apps.snaq(tree_method=basedir["tree_method"], 
+                        gen_tree=besttree_file, spec_tree=spec_tree,
+                        output_folder=output_folder, num_threads=1, 
+                        hmax=h, runs=config.snaq_runs, astral=r_astral))
     compss_wait_on(r_snaq)
+
+
 if __name__ == "__main__":
     raxml_snaq()
